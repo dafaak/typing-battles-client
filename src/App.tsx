6fad266e83d8dd7ec82a2f7fcb620b17e5190e16
dafaak <23407589+dafaak@users.noreply.github.com>
2025-12-30 12:53:06 -0500

    refactor: switch to BrowserRouter for improved routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { Loby } from './pages/Loby';
-import { HashRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+import { BrowserRouter, HashRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { useEffect } from "react";
 import { GameState, GameStatus } from "./interfaces/GameStatus.ts";
@@ -56,13 +56,13 @@ function App() {
   }, [socket, dispatch, navigate]); // Añade 'navigate' a las dependencias
 
   return (
-      <Router>
+      <BrowserRouter basename='/typing-battles-client'>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/loby" element={<Loby/>}/>
           <Route path="/game" element={<Game/>}/>
         </Routes>
-      </Router>
+      </BrowserRouter>
   )
 }
 
