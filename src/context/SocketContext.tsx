571ca31bc90d8f8ef67b4196fc8c5191f6b2b841
Israel <israel.pe@outlook.es>
2025-12-30 12:19:12 -0500

    chore: cahnge server url

--- src/context/SocketContext.tsx
+++ src/context/SocketContext.tsx
@@ -1,7 +1,7 @@
 import {createContext, useContext, ReactNode} from 'react';
 import {io, Socket} from 'socket.io-client';
 
-const URL = 'http://localhost:8080';
+const URL = 'https://typing-battles-server.onrender.com';
 const socket: Socket = io(URL);
 
 const SocketContext = createContext<Socket>(socket);
